c2ffec5dbc7752d1f7f45c55c56b3103fb4b2c51
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-25 22:14:24 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,7 +59,15 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     array = programmer_hash[:dennis_ritchie][:languages]
+     
+return array[0]
+
+
 end
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -80,6 +92,10 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+
+return programmer_hash
+
     
 end
 
@@ -102,6 +118,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     return programmer_hash
      
 end
 
@@ -121,5 +140,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+return programmer_hash
 
 end
